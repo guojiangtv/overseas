e8e2d1e49c3fd21e9032ced38ca5cab135f5fd97
leaf <leaf930813@163.com>
2018-05-07 15:14:07 +0800

    完善编译环境

--- config/resolve.config.js
+++ config/resolve.config.js
@@ -1,17 +1,20 @@
 const path = require('path');
-const vue_source = process.env.NODE_ENV === 'production' ? path.resolve(__dirname, './../node_modules/vue/dist/vue.min.js') : path.resolve(__dirname, './../node_modules/vue/dist/vue.js');
+
+var pcBaseEntryDir = '../src/v2/pc/';
+var mobileBaseEntryDir = '../src/v2/mobile/';
+
+var baseEntryDir = process.env.PLATFORM == 'pc' ? pcBaseEntryDir : mobileBaseEntryDir;
+
+const vue_source = process.env.NODE_ENV === 'production' ? path.resolve(__dirname, baseEntryDir + 'js/lib/vue.min.js') : path.resolve(__dirname, baseEntryDir + 'js/lib/vue.js');
 
 module.exports = {
     // 模块别名的配置，为了使用方便，一般来说所有模块都是要配置一下别名的
     alias: {
         'vue': vue_source,
-        'axios': path.resolve(__dirname, './../node_modules/axios/dist/axios.min.js'),
-        'layer': path.resolve(__dirname, '../src/v2/mobile/js/lib/layer.js'),
-        'jquery':path.resolve(__dirname, './../node_modules/jquery/dist/jquery.min.js'),
-        'webpack-zepto': path.resolve(__dirname, './../node_modules/webpack-zepto/index.js'),
-        'common': path.resolve(__dirname, '../src/v2/mobile/js/common/common.js'),
-        'pc_common': path.resolve(__dirname, '../src/v2/pc/js/common/common.js'),
-
+        //'jquery':path.resolve(__dirname, baseEntryDir + 'js/lib/jquery.min.js'),
+        'axios': path.resolve(__dirname, baseEntryDir + 'js/lib/axios.min.js'),
+        'layer': path.resolve(__dirname, baseEntryDir + 'js/lib/layer.js'),
+        'common': path.resolve(__dirname, baseEntryDir + 'js/common/common.js'),
     },
 
     // 当require的模块找不到时，尝试添加这些后缀后进行寻找
